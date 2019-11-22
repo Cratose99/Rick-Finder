ff1ef20210ac1a72ab5bd5a5ba20c7bb5fe9db84
Cratose99 <gabriel.eyer007@gmail.com>
2019-11-22 03:37:21 -0800

    yo added the friends

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+    {
+        name: "Rick C-137",
+        scores: [5, 5, 1, 5, 5, 5],
+        imageSource: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+        alt: "Rickest Rick"
+    },
+    {
+        name: "Simple Rick",
+        scores: [3, 1, 5, 2, 4, 2],
+        imageSource: "https://vignette.wikia.nocookie.net/rickandmorty/images/8/8c/Howyoudoin%27bb.png/revision/latest?cb=20170912092452",
+        alt: "Simple Man"
+    },
+    {
+        name: "Pickle Rick",
+        scores: [5, 5, 1, 5, 1, 2],
+        imageSource: "https://vignette.wikia.nocookie.net/rickandmorty/images/1/19/Pickle_rick_transparent.png/revision/latest?cb=20171025014216",
+        alt: "Pickle"
+    },
+    {
+        name: "Dumb Rick",
+        scores: [5, 1, 5, 1, 1, 5],
+        imageSource: "https://vignette.wikia.nocookie.net/rickandmorty/images/5/57/Rick_J19-Zeta_7.png/revision/latest?cb=20140408161953",
+        alt: "DUhhhhhh"
+    }
+]
+
+module.exports = friends;
